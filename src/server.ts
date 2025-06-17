fefedd38dc170650aaf85db5f92e75143ecf460e
M2 C1 <hienphannguyenxuan@gmail.com>
2025-06-17 11:44:27 +0700

    new save mess 2

--- src/server.ts
+++ src/server.ts
@@ -36,13 +36,13 @@ io.on('connection',(socket)=>{
   })
 
   socket.on("sendMessage",async(message)=>{
-    const {conversationId,senderId,content}= message;
+    const {conversation_id,sender_id,content}= message;
 
     try{
-      const savedMessage = await saveMessage(conversationId,senderId,content)
+      const savedMessage = await saveMessage(conversation_id,sender_id,content)
       console.log("sendMessage: ");
-      console.log(saveMessage);
-io.to(conversationId).emit("newMessage",savedMessage);
+      console.log(savedMessage);
+io.to(conversation_id).emit("newMessage",savedMessage);
     }catch(error){
       console.log(error);
     };
