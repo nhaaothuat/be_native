643178d7140f5e54746294152b06b78607e47f00
M2 C1 <hienphannguyenxuan@gmail.com>
2025-06-18 15:10:00 +0700

    new mess3

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import authRoute from "./route/authRoute.js";
 import converRoute from "./route/converRoute.js";
-import messRoute from "./route/messageRoute.js"
+import messRoute from "./route/messageRoute.js";
 import { initDB } from "./model/intitDB.js";
 import { createServer } from "http";
 import { Server } from "socket.io";
@@ -13,46 +13,55 @@ const PORT = process.env.PORT;
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors:{
-    origin:'*'
-  }
+  cors: {
+    origin: "*",
+  },
 });
-
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/conver", converRoute);
-app.use("/api/v1/message",messRoute)
+app.use("/api/v1/message", messRoute);
 
+io.on("connection", (socket) => {
+  console.log("A user connected: ", socket.id);
 
-io.on('connection',(socket)=>{
-  console.log("A user connected: ",socket.id);
+  socket.on("joinConversation", (conversationId) => {
+    socket.join(conversationId);
+    console.log("User joined conversation: ", conversationId);
+  });
 
-  socket.on("joinConversation",(conversationId)=>{
-    socket.join(conversationId)
-    console.log("User joined conversation: ",conversationId)
-  })
+  socket.on("sendMessage", async (message) => {
+    const { conversation_id, sender_id, content } = message;
 
-  socket.on("sendMessage",async(message)=>{
-    const {conversation_id,sender_id,content}= message;
-
-    try{
-      const savedMessage = await saveMessage(conversation_id,sender_id,content)
+    try {
+      const savedMessage = await saveMessage(
+        conversation_id,
+        sender_id,
+        content
+      );
       console.log("sendMessage: ");
       console.log(savedMessage);
-io.to(conversation_id).emit("newMessage",savedMessage);
-    }catch(error){
-      console.log(error);
-    };
-    
-  })
+      io.to(conversation_id).emit("newMessage", savedMessage);
 
-  socket.on("disconnect",()=>{
-    console.log("User disconnected: ",socket.id);
-  })
-})
+
+      io.emit("conversationUpdated",{
+        conversation_id,
+        lastMessage:savedMessage.content,
+        lastMessageTime:savedMessage.created_at
+      })
+
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected: ", socket.id);
+  });
+});
 
 initDB().then(() => {
   httpServer.listen(PORT, () => {
