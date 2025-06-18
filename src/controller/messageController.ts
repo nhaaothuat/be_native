b89ba34b96cf1d51a4099af0f143f22fe7d0c6d7
M2 C1 <hienphannguyenxuan@gmail.com>
2025-06-18 13:56:51 +0700

    new sql

--- src/controller/messageController.ts
+++ src/controller/messageController.ts
@@ -1,35 +1,36 @@
 import { sql } from "../utils/db.js";
 import TryCatch from "../utils/TryCatch.js";
 
-export const fetchAllMessageByConversationId = TryCatch(async(req,res)=>{
-let userId = null;
-if(req.user){
-  userId = req.user.id;
-}
+export const fetchAllMessageByConversationId = TryCatch(async (req, res) => {
+  let userId = null;
+  if (req.user) {
+    userId = req.user.id;
+  }
 
-const results = await sql`
-SELECT c.id AS coversation_id,
-CASE
-WHEN u1.id=${userId} THEN u2.username
-ELSE AS participant_name,
-m.content AS last_message
-m.created_at AS last_message_time
+  const results = await sql`
+SELECT c.id AS conversation_id,
+  CASE
+    WHEN u1.id = ${userId} THEN u2.username
+    ELSE u1.username
+  END AS participant_name,
+  m.content AS last_message,
+  m.created_at AS last_message_time
 FROM conversations c
-JOIN users u1 ON u1.id=c.participant_one
-JOIN users u2 ON u2.id=c.participant_two
-LEFT JOIN LATERIAL(
-SELECT content,created_at
-FROM messages
-WHERE conversation_id=c.id
-ORDER BY created_at DESC
-LIMIT 1
+JOIN users u1 ON u1.id = c.participant_one
+JOIN users u2 ON u2.id = c.participant_two
+LEFT JOIN LATERAL (
+  SELECT content, created_at
+  FROM messages
+  WHERE conversation_id = c.id
+  ORDER BY created_at DESC
+  LIMIT 1
 ) m ON true
- WHERE c.participant_one=${userId} or c.participant_two=${userId}
- ORDER BY m.created_at DESC;
-`
+WHERE c.participant_one = ${userId} OR c.participant_two = ${userId}
+ORDER BY m.created_at DESC;
+`;
 
-res.json(results);
-})
+  res.json(results);
+});
 
 export const saveMessage = async (
   conversationId: string,
